47f8d14b3dfe96ef64f54709cf08e0adf3b359c2
gteixi <gemmateixido25@gmail.com>
2025-05-23 12:44:35 +0200

    feat(types): add global types

---
+++ src/data/products.ts
@@ -0,0 +1,40 @@
+import { Product } from '@/types/product';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 'cerave',
+    name: 'Cerave ® Crema Hidratante 340ml',
+    price: 11.7,
+    image: '/images/products/cerave-crema.jpg',
+  },
+  {
+    id: 'fotoprotector',
+    name: 'Fotoprotector Solar ISDIN® Fusion Water SPF 50+ 50ml',
+    price: 12.9,
+    image: '/images/products/fotoprotector-isdin.jpg',
+  },
+  {
+    id: 'hyabak',
+    name: 'Hyabak Solución Oftálmica 10ml',
+    price: 10.5,
+    image: '/images/products/hyabak-solucion.jpg',
+  },
+  {
+    id: 'lajusticia',
+    name: 'Ana María Lajusticia Colágeno con Magnesio 350g',
+    price: 15.2,
+    image: '/images/products/lajusticia-colageno.jpg',
+  },
+  {
+    id: 'piz',
+    name: 'Piz Buin® Protector Solar SPF 30 200ml',
+    price: 13.8,
+    image: '/images/products/piz-buin.jpg',
+  },
+  {
+    id: 'xhekpon',
+    name: 'Xhekpon® Crema Facial 40ml',
+    price: 8.9,
+    image: '/images/products/xhekpon-crema.jpg',
+  },
+];
