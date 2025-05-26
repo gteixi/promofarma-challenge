8c9cd4a2aa81204ad8fbba5763c421838144e218
gteixi <gemmateixido25@gmail.com>
2025-05-26 11:38:18 +0200

    test: test components

---
+++ src/components/ProductList/__tests__/ProductList.test.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react';
+import ProductList from '../ProductList';
+import renderWithProviders from '@/__tests__/testUtils/renderWithProviders';
+import { mockProducts } from '@/__tests__/testUtils/mockProducts';
+
+describe('ProductList', () => {
+  it('renders a list of products', () => {
+    renderWithProviders(<ProductList />);
+    mockProducts.forEach((product) => {
+      expect(screen.getByText(product.name)).toBeInTheDocument();
+    });
+  });
+});
