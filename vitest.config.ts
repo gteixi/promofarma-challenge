140e8950b9df2eede45839a1a2212d0538a13a34
gteixi <gemmateixido25@gmail.com>
2025-05-26 11:37:35 +0200

    chore: set vitest configuration

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  esbuild: {
+    jsx: 'automatic',
+  },
+});
